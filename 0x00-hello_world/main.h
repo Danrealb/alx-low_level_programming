32e6a4f2b09280f60b67dda6a06292dfccda512e
Danrealb <danrealben@gmail.com>
2022-07-12 15:41:36 -0700

    redit

--- 0x00-hello_world/main.h
+++ 0x00-hello_world/main.h
@@ -1,16 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-
-#endif
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: the character to be checked
+ *                                                 * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+        return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
