ad7e595d3fb67ae1fa4ce17e03351ba28ff44745
Sunday_Attah-Daniel <danrealben@gmail.com>
2022-09-09 12:37:30 -0700

    bounce

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Prints quote to the standard error
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+	write("and that piece of art is useful - Dora Korpar, 2015-10-19,\n");
+	return (1);
+}
