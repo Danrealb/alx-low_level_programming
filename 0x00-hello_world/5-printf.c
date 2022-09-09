f1d38aaee2418a4dbd67cc5dbbce4a73307102f4
Sunday_Attah-Daniel <danrealben@gmail.com>
2022-09-09 12:14:30 -0700

    trailing whitespace

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Print quote
+ * main - Prints quote
  *
- * return 0 success
- *
- * Description - proper grammar, but the outcome is a piece of art,
+ * Return: zero on success
  *
  */
-
 int main(void)
-
 {
-	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
