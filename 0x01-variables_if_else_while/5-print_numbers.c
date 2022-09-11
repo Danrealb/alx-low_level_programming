34b6584a95a8339d6df32fe32299224fda3e322a
Sunday_Attah-Daniel <danrealben@gmail.com>
2022-09-11 14:47:36 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Print base 10 numbers
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints number 0-9
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char num = '0';
+
+	while
+	(num <= '9');
+
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
