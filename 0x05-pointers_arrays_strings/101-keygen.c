c3dbc792837b106c0e39f16424e8c32bd22789cb
Sunday_Attah-Daniel <danrealben@gmail.com>
2022-09-20 12:26:35 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,55 +1,24 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-#include "main.h"
+#include <math.h>
+#include <time.h>
 
-
-/**
- * keygen - generating a random passwords
- *
- *
- **/
-
-int password(int password_length) 
-
+int main()
 {
-	char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/\|";
-	printf("\t");
-	for(int i = 0; i < password_length; i++) 
-	{
-		printf("*");
-	}
-	
-	printf("\n");
-	printf("\t");
-	srand(time(NULL));
-	for(int i = 0; i < password_length; i++);
-	{
-		printf("%c", list[rand() % (sizeof list - 1)]);
-	}
+    srand((unsigned int)(time(NULL)));
+    int i;
+    char pass[12];
 
-	printf("\n");
-	printf("\t");
-	for(int i = 0; i < password_length; i++) 
-	
-	{
-	printf("*");
-	}
+    printf("Press enter to get a twelve-character password\n");
+    getchar();
 
-	printf("\n");
-	}
-
-	int main() 
-	
-	{
-	int password_length;
-	printf("\n\t*********************************\n\n");
-	printf("\tWelcome to the password generator\n\n");
-	printf("\t*********************************\n");
-	printf("\n\tEnter length of the password = ");
-	scanf("%d", &password_length);
-	printf("\n");
-	printf("\n");
-	password(password_length);
-	return 0;
+    for (i = 0; i < 4; i++) {
+        pass[i] = rand() % 9;
+        char capLetter = 'A' + (rand() % 26);
+        pass[i + 2] = capLetter;
+        char letter = 'a' + (rand() % 26);
+        pass[i + 3] = letter;
+        printf("%d%c%c", pass[i], pass[i + 2], pass[i + 3]);
+    }
+    printf("\n\n");
 }
