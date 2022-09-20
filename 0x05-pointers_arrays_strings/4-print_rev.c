af973cb6721c9802d4f6cb8227bcdbc41cbfe45a
Sunday_Attah-Daniel <danrealben@gmail.com>
2022-09-20 11:32:14 -0700

    answer

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,14 @@
  * Return: nothing
  **/
 
-void print_rev(char *s)
+void _puts(char *str)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
 		i++;
-
-	for (i = i - 1; i >= 0; i--)
-		_putchar(s[i]);
-
-	_putchar('\n');
+	}
+	putchar('\n');
 }
