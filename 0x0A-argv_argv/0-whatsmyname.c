4914f09f6fcb728a5cafc8f98b56df2f40247c06
Sunday_Attah-Daniel <danrealben@gmail.com>
2022-09-30 10:15:50 -0700

    print the name of a file

---
+++ 0x0A-argv_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints name of file
+ *@argc: number of arguments
+ * @argv: arguments entered
+ * Return - 0 always
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	argc = argc;
+
+	return (0);
+
+}
---
+++ 0x0A-argv_argv/README.md
@@ -0,0 +1,7 @@
+int main(int argc, char *argv[])
+
+	printf("%s\n", argv[0]);
+	argc = argc;
+
+	return (0);
+first project : print the name of a file	
